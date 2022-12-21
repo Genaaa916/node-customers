4617fcca165a90a435a50df3c93a7592499f3e50
Gena <genci.behxheti@gmail.com>
2022-12-21 16:26:42 +0200

    changed movies to customers

--- index.js
+++ index.js
@@ -4,46 +4,46 @@ const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
-// movies array
-let movies = [
-  {id: '1588323375416', title: 'Star Wars: Episode IX - The Rise of Skywalker', year: 2019, director: 'J.J. Abrams'},
-  {id: '1588323390624', title: 'The Irishman', year: 2019, director: 'Martin Scorsese'},
-  {id: '1588323412643', title: 'Harry Potter and the Sorcerers Stone', year: 2001, director: 'Chris Columbus'}
-];
+// customers array
+let customers = [
+  {id: '1588323375416', firstName: 'John', lastName: 'Johnson', email: 'john@johnson.com', phone: '8233243'},
+  {id: '1588323375417', firstName: 'Mary', lastName: 'Smith', email: 'mary@smith.com', phone: '6654113'},
+  {id: '1588323375418', firstName: 'Peter', lastName: 'North', email: 'peter@north.com', phone: '901176'},
+]
 
-// get all movies
-app.get('/api/movies', (req, res) => {
-    res.json(movies);
+// get all customers
+app.get('/api/customers', (req, res) => {
+    res.json(customers);
 });
 
-// delete movie by id
-app.delete('/api/movies/:id', (req, res) => {
+// delete customer by id
+app.delete('/api/customers/:id', (req, res) => {
     const id = req.params.id;
-    movies = movies.filter(movie => movie.id !== id);
+    customers = customers.filter(customer => customer.id !== id);
     res.status(204).end();
 });
 
-//update movie by id
-app.put('/api/movies/:id', (req, res) => {
+//update customers by id
+app.put('/api/customers/:id', (req, res) => {
     const id = req.params.id;
-    const updatedMovie = {...req.body, id: id};
-    const index = movies.findIndex(movie => movie.id === id);
-    movies.splice(index, 1, updatedMovie);
-    res.json(updatedMovie);
+    const updatedcustomer = {...req.body, id: id};
+    const index = customers.findIndex(customer => customer.id === id);
+    customers.splice(index, 1, updatedcustomer);
+    res.json(updatedcustomer);
 });
-// add new movie
-app.post('/api/movies', (req, res) => {
-    const movie = req.body;
-    movies = [...movies, movie];
-    res.json(movie);
+// add new customer
+app.post('/api/customers', (req, res) => {
+    const newCustomer = {'id': Date.now().toString(), ...req.body};
+    customers = [...customers, newCustomer];
+    res.json(newCustomer);
 });
 
-//get movie by id
-app.get('/api/movies/:id', (req, res) => {
+//get customer by id
+app.get('/api/customers/:id', (req, res) => {
     const id = req.params.id;
-    const movie = movies.filter(movie => movie.id === id);
-    if (movie.length > 0) {
-        res.json(movie);
+    const customer = customers.filter(customer => customer.id === id);
+    if (customer.length > 0) {
+        res.json(customer);
     } else {
         res.status(404).end();
 }});
