8a4cc3c022dc185ca1eeca4a1fa10ab2fb4dea59
Gena <genci.behxheti@gmail.com>
2023-01-27 19:21:33 +0200

    added bootstrap

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const feedDisplay = document.querySelector("#feed")
+const url = "http://localhost:3000/api/customers"
 
-fetch("http://localhost:3000/api/customers")
+
+fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log(data)
         data.forEach(customer => {
-            feedDisplay.innerHTML += `<h3> ${customer.firstname} ${customer.lastname} </h3> `
+            feedDisplay.innerHTML += `<h3 class="text-danger border-bottom p-2" > ${customer.firstname} ${customer.lastname} </h3>
+            <p> ${customer.email} </p>
+            <p> ${customer.phone} </p>
+             `
         })
     })
